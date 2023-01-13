a2396953e0f19d1cf71323020d0f1d5ac658d744
Tobi-D7 <tobid7@outlook.de>
2023-01-13 14:43:57 +0100

    Test

--- source/napp/worker.cpp
+++ source/napp/worker.cpp
@@ -1,45 +1,38 @@
 #include <3ds.h>
 #include <iostream>
+#include <map>
 #include <napp/worker.hpp>
 #include <nuseful/stringtools.hpp>
 
-struct w_Task {
-  std::string name;
-  Thread thrd;
-};
-
-std::vector<w_Task> tasks;
+std::map<std::string, Thread *> tasks;
 
 namespace nlc {
 namespace worker {
 void push(void (*task)(void), std::string name) {
-  w_Task temp;
-  temp.name = nlc::st::ToLowerCase(name);
+  std::string __name = nlc::st::ToLowerCase(name);
   s32 prio = 0;
   svcGetThreadPriority(&prio, CUR_THREAD_HANDLE);
-  temp.thrd =
+  Thread thrd =
       threadCreate((ThreadFunc)task, NULL, 64 * 1024, prio - 1, -2, false);
-  tasks.push_back(temp);
+  tasks.insert(std::make_pair(__name, &thrd));
 }
 std::vector<std::string> GetTasks() {
   std::vector<std::string> temp;
   for (auto const &it : tasks) {
-    temp.push_back(it.name);
+    temp.push_back(it.first);
   }
   return temp;
 }
 void kill(std::string task) {
-  for (auto const &it : tasks) {
-    if (it.name == nlc::st::ToLowerCase(task)) {
-      threadJoin(it.thrd, U64_MAX);
-      threadFree(it.thrd);
-    }
+  if (tasks.contains(nlc::st::ToLowerCase(task))) {
+    threadJoin(tasks.at(nlc::st::ToLowerCase(task))[0], U64_MAX);u
+    threadFree(tasks.at(nlc::st::ToLowerCase(task))[0]);
   }
 }
 void exit() {
   for (auto const &it : tasks) {
-    threadJoin(it.thrd, U64_MAX);
-    threadFree(it.thrd);
+    threadJoin(it.second[0], U64_MAX);
+    threadFree(it.second[0]);
   }
 }
 
